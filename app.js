995c9dc1c1d7873c5b3615400d6790c4098454e2
aleks-123 <ace.ki@hotmail.com>
2022-09-27 02:01:01 +0200

    Added a readme and modified app comment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRoutes = require('./routes/viewRoutes');
-// Start express app Yess
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
